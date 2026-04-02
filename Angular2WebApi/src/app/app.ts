98927879af336cc8dd2d8d7203606e086d97a835
priy-nshu <priyanshudwibed6@gmail.com>
2026-04-02 17:36:29 +0530

    fixes

--- Angular2WebApi/src/app/app.ts
+++ Angular2WebApi/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentService } from './services/student-service';
 
 @Component({
   selector: 'app-root',
