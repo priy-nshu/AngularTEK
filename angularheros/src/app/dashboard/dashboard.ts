26c6c141b102a277be7d7b916c2d718cf4aeb83e
priy-nshu <priyanshudwibed6@gmail.com>
2026-04-02 10:53:44 +0530

    Routing fixed

--- angularheros/src/app/dashboard/dashboard.ts
+++ angularheros/src/app/dashboard/dashboard.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from '../Hero';
 import { HeroService } from '../hero-service';
 import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
@@ -20,7 +21,7 @@ export class Dashboard {
 
   ComponentgetHeroes(): void {
     this.myHeroSvc.GetTopHeros()
-      .subscribe(hlist => this.myheroes = hlist);
+    .subscribe(hlist => this.myheroes = hlist);
 
     console.log(this.myheroes);
   }
--- angularheros/src/app/hero-details/hero-details.ts
+++ angularheros/src/app/hero-details/hero-details.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from '../Hero';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Hero } from '../Hero';
 import { HeroService } from '../hero-service';
 
 @Component({
@@ -9,17 +9,23 @@ import { HeroService } from '../hero-service';
   templateUrl: './hero-details.html',
   styleUrl: './hero-details.css',
 })
-export class HeroDetails {
- // @Input() myHero?:Hero
- myHero?: any;
- constructor(private myHeroSvc: HeroService, private route: ActivatedRoute){}
- 
- ngOnInit():void{
-  this.ComponentgetHero();
- }
-  ComponentgetHero():void{
-  const id = Number(this.route.snapshot.paramMap.get('id'));
-  this.myHeroSvc.GetHeros()
-    .subscribe( h1=>this.myHero =h1);
-    };
+export class HeroDetails implements OnInit {
+
+  myHero?: Hero;
+
+  constructor(
+    private myHeroSvc: HeroService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.ComponentgetHero();
+  }
+
+  ComponentgetHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.myHeroSvc.GetHeroId(id)
+      .subscribe((h1) => this.myHero = h1);
+  }
 }
