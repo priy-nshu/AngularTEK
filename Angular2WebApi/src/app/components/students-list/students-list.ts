7e442198e6a43a20d5e20c9f431bc9479d5662e4
priy-nshu <priyanshudwibed6@gmail.com>
2026-04-03 14:08:58 +0530

    BE-FE

--- Angular2WebApi/src/app/components/students-list/students-list.ts
+++ Angular2WebApi/src/app/components/students-list/students-list.ts
@@ -1,25 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core'; // Added OnInit and inject
 import { FormsModule } from '@angular/forms';
 import { Student } from '../../models/student.model';
 import { StudentService } from '../../services/student-service';
 import { CommonModule } from '@angular/common';
-import { Router,RouterLink } from '@angular/router';
+import { Router, RouterLink, ActivatedRoute } from '@angular/router'; // Added ActivatedRoute
 
 @Component({
   selector: 'app-students-list',
+  standalone: true,
   imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './students-list.html',
   styleUrl: './students-list.css',
 })
-export class StudentsList {
+export class StudentsList implements OnInit {
   stdList: Student[] = [];
-  constructor(private studentService: StudentService,private router :Router) {}
+  
+  // Using modern 'inject' or constructor injection works
+  private studentService = inject(StudentService);
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
 
   ngOnInit() {
+    // Corrected syntax: Just call getData when the component loads
     this.getData();
+    
+    // If you specifically need to react to route data changes:
+    this.activatedRoute.data.subscribe({
+      next: (data) => {
+        console.log('Route data:', data);
+        this.getData();
+      }
+    });
   }
   
-  getData() {
+getData() {
     this.studentService.getStudents()
         .subscribe({next: (data) => {this.stdList = data;
         console.log(data);},
